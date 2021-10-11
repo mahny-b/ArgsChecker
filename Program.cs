8fa193e574d838cc2e3f5d26726e62fe7b3e7d9f
mahny-b <14142681+mahny-b@users.noreply.github.com>
2021-10-11 22:26:02 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArgsChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
